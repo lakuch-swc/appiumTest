0b4bda7aa4a58d2e7853130cf654dd46eeb7a665
La Kuch <la.kuch@students.williscollege.com>
2019-06-04 16:46:06 -0400

    Added searchTest, removed search.

---
+++ src/test/java/appiumTest/BaseClass.java
@@ -0,0 +1,53 @@
+package appiumTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.BeforeTest;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class BaseClass {
+
+	static AppiumDriver driver;
+	
+	@BeforeTest
+	public void setup() {
+		
+		DesiredCapabilities dc = new DesiredCapabilities();
+		
+		dc.setCapability(MobileCapabilityType.PLATFORM_NAME, "ANDROID");
+		dc.setCapability(MobileCapabilityType.PLATFORM_VERSION, "8.0.0");
+		dc.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel");
+		dc.setCapability(MobileCapabilityType.UDID, "emulator-5554");
+		dc.setCapability(MobileCapabilityType.NEW_COMMAND_TIMEOUT, 30);
+        dc.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.mindshare.magnifi");
+        //dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".view.SplashScreenActivity");
+        dc.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, ".view.ExpertSearchActivity");
+        dc.setCapability("autoGrantPermissions", true);
+        dc.setCapability("autoAcceptAlerts", true);
+        dc.setCapability("autoDismissAlerts", true);
+        dc.setCapability("noReset", true);
+
+		try {
+			URL url = new URL("http://127.0.0.1:4723/wd/hub");
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getCause());
+			System.out.println(e.getMessage());;
+			e.printStackTrace();
+		}
+
+		driver = new AppiumDriver<MobileElement>(dc);
+		
+	}
+	
+	public void teardown() {
+		driver.close();
+		driver.quit();
+	}
+}
