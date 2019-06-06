d57b7a71e0823ca3bfd60e9a41817ce4e184802f
La Kuch <la.kuch@students.williscollege.com>
2019-06-06 13:42:01 -0400

    Moved screenshot method to BaseClass.java

--- src/test/java/appiumTest/searchTest.java
+++ src/test/java/appiumTest/searchTest.java
@@ -4,11 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.UUID;
-
-import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -20,13 +16,6 @@ public class searchTest extends BaseClass {
 	File userFile = new File("users.txt");
 	String screenshotLocation = "C:\\Users\\student\\Desktop\\";
 	
-	public void screenshot(String path_screenshot) throws IOException {
-	    File srcFile = driver.getScreenshotAs(OutputType.FILE);
-	    String filename = UUID.randomUUID().toString(); 
-	    File targetFile = new File(path_screenshot + filename +".jpg");
-	    FileUtils.copyFile(srcFile, targetFile);
-	}
-	
 	@Test
 	public void searchTextFileForUser() throws IOException, InterruptedException {
 		
@@ -36,13 +25,25 @@ public class searchTest extends BaseClass {
 			String user;
 			while ((user = bufferedReader.readLine()) != null) {
 			
-				new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/drawable_left_icon"))).click();
+				try {
+					new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/drawable_left_icon"))).click();
+					Thread.sleep(2000);
+				} finally {
+					screenshot(screenshotLocation);
+				}
+				
 				new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.TextView[@text='Search']"))).click();
 				new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/expertSearchEditText"))).sendKeys(user);
-				driver.executeScript("mobile: performEditorAction", ImmutableMap.of("action", "Search"));
-				new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/mainLayout")));
-				Thread.sleep(5000);
 				screenshot(screenshotLocation);
+				driver.executeScript("mobile: performEditorAction", ImmutableMap.of("action", "Search"));
+				
+				try {
+					new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.className("android.widget.RelativeLayout")));
+					Thread.sleep(2000);
+				} finally {
+					screenshot(screenshotLocation);
+				}
+				
 			}
 			
 		} finally {
