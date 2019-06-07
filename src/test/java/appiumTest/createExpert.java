04df40e88ed59224dc21581bd8bb02270d89b202
Oksana Miller <milleroks1992@gmail.com>
2019-06-07 15:18:50 -0400

    expert withoun bank

--- src/test/java/appiumTest/createExpert.java
+++ src/test/java/appiumTest/createExpert.java
@@ -19,52 +19,79 @@ public class createExpert extends BaseClass {
 		//Test is supposed to run after running createUser test, and verifying email address
 		// When user is logged in SplashScreen starts at ExpertSearchActivity
 		
-		//Click Become an expert link
-		 new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/expertTextView")));
-         MobileElement becomeExpert = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/expertTextView"));
-         becomeExpert.click();
-         //Click Skip
-         new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/joinTextView")));
-         MobileElement skip = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/joinTextView"));
-         skip.click();
-         
-         //Step 1: Create expert profile
-         //Enter title
-         new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/titleEditText")));
-         MobileElement etTitle = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/titleEditText"));
-         etTitle.clear();
-         etTitle.sendKeys(strTitle + enter);
-         //Enter description
-         new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/descriptionEditText")));
-         MobileElement etDescription = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/descriptionEditText"));
-         etDescription.clear();
-         etDescription.sendKeys(strDescription + enter);
-         driver.hideKeyboard();
-         //Add category. Expert has to add at least 1 category (up to 3)
-         new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/categoryTextView")));
-         MobileElement addCategory = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/categoryTextView"));
-//         addCategory.click();
-//         new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Art']")));
-//         MobileElement category = (MobileElement) driver.findElement(By.xpath("//*[@text='Art']"));
-//         category.click();
-         nextStep();    
-         //Add tags (adding tags are optional)
-        // addTags();
+		 navigateToBecomeExpertScreen();
+	         
+         //Step 1: Create expert profile. Adding tags are optional
+		 enterExpertiseInfo();
+		 addCategory();       
+         // addTags();
          nextStep();
          
          //Step 2: add social feeds (optional)
          //addSocialFeeds();
          nextStep();
          
-         //Step 3: Intro video (mandatory)
-         new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/cameraLinearLayout")));
-         MobileElement addMedia = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/cameraLinearLayout"));
-         addMedia.click();
+         //Step 3: Intro video (mandatory): Choose choose the method
+         camera.openCamera(driver);
+         camera.takePhoto(driver);
          //camera.takeVideo(driver);
-         //camera.takePhoto();
-         camera.chooseFromLibrary(driver);
+         //camera.chooseFromLibrary(driver);
+         nextStep();
+         
+         //Step 4: Set rate and minutes (optional)
+         //setRate();
+         //setFreeMinutes();
+         nextStep();
+         
+         //Step 5: Set Availability (optional)
+         //setAvailability();
+         nextStep();
+         
+         //Step 6: Add bank account (mandatory)
+         addBankAccount();
+         
+         //Step 7: Preview & publish profile
+         previewAndPublish();
+         
+         
 	}
 	
+	public static void navigateToBecomeExpertScreen(){
+		//Click Become an expert link
+		 new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/expertTextView")));
+        MobileElement becomeExpert = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/expertTextView"));
+        becomeExpert.click();
+        //Click Skip
+        new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/joinTextView")));
+        MobileElement skip = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/joinTextView"));
+        skip.click();	
+	}
+	
+	public static void  enterExpertiseInfo() {
+		//Enter title
+        new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/titleEditText")));
+        MobileElement etTitle = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/titleEditText"));
+        etTitle.clear();
+        etTitle.sendKeys(strTitle + enter);
+        //Enter description
+        new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/descriptionEditText")));
+        MobileElement etDescription = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/descriptionEditText"));
+        etDescription.clear();
+        etDescription.sendKeys(strDescription + enter);
+        driver.hideKeyboard();
+	}
+	
+	public static void addCategory() {
+		//Add category. Expert has to add at least 1 category (up to 3)
+        new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/categoryTextView")));
+        MobileElement addCategory = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/categoryTextView"));
+        addCategory.click();
+        new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Art']")));
+        MobileElement category = (MobileElement) driver.findElement(By.xpath("//*[@text='Art']"));
+        category.click();
+        nextStep();
+	}
+
 	public static void nextStep(){
 		   new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/nextTextView")));
 	       MobileElement save = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/nextTextView"));
@@ -72,7 +99,7 @@ public class createExpert extends BaseClass {
 	}
 	
 	public static void addTags(){
-		 new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/tagTextView")));
+		new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/tagTextView")));
         MobileElement addTag = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/tagTextView"));
         addTag.click();
         new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/tagEditText")));
@@ -84,6 +111,29 @@ public class createExpert extends BaseClass {
 	public static void addSocialFeeds(){
 		
 	}
+	
+	public static void addBankAccount(){
+		//Click Add Bank account button
+		new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/addCreditCardButton")));
+        MobileElement addBankAccount = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/addCreditCardButton"));
+        addBankAccount.click();
+        //Click development mode: skip
+        new WebDriverWait(driver, 120).until(ExpectedConditions.presenceOfElementLocated(By.id("By.xpath(\"//*[@text='Skip this account form']\")")));
+        MobileElement skip = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/Skip this account form"));
+        skip.click();
+	}
+	
+	public static void previewAndPublish(){
+		//Preview
+		new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/logInButton")));
+        MobileElement preview = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/logInButton"));
+        preview.click();
+        //Publish
+        new WebDriverWait(driver, 30).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/publishButton")));
+        MobileElement publish = (MobileElement) driver.findElement(By.id("com.mindshare.magnifi:id/publishButton"));
+        publish.click();       
+	}
+	
 }
 
 
