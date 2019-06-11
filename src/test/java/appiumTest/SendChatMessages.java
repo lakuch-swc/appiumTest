e2dd3948e7fc7765e6929dc1724debe8310b305a
SZawie <zswhatever@gmail.com>
2019-06-11 10:56:26 -0400

    Code Update/CleanUp

--- src/test/java/appiumTest/SendChatMessages.java
+++ src/test/java/appiumTest/SendChatMessages.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class SendChatMessages extends BaseClass {
-	final String screenshotsFolder = "C:\\Users\\student\\Desktop\\";
+	final String screenshotsFolder    = "C:\\Users\\student\\Desktop\\";
+	final String screenshotsSubFolder = "Salah";
+	
 	final String userName   = "salahdau@yahoo.com";
 	final String passWord   = "Abcd1234";
 	
@@ -18,41 +20,41 @@ public class SendChatMessages extends BaseClass {
     public void testSendChatMessages() throws IOException, InterruptedException {
     	// Make sure user is logged in.
 	 	login(userName, passWord);
-	   	screenshot(screenshotsFolder);
+	   	screenshot(screenshotsFolder, screenshotsSubFolder);
         
 	   	// Enter Keyword or Expert_Name to Search
         new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/expertSearchEditText"))).sendKeys(expertName);
-        screenshot(screenshotsFolder);
+        screenshot(screenshotsFolder, screenshotsSubFolder);
         
         driver.executeScript("mobile: performEditorAction", ImmutableMap.of("action", "Search"));
-        screenshot(screenshotsFolder);
+        screenshot(screenshotsFolder, screenshotsSubFolder);
         
         // Click Chat image/icon.
         new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/chatImageView"))).click();
-        screenshot(screenshotsFolder);
+        screenshot(screenshotsFolder, screenshotsSubFolder);
 
         new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/chatImageView"))).click();
-        screenshot(screenshotsFolder);
+        screenshot(screenshotsFolder, screenshotsSubFolder);
 
         // Enter a text message to send to the Expert.
         new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//android.widget.EditText"))).sendKeys("Hi " + expertName + ", my name is Salah.");     
-        screenshot(screenshotsFolder);
+        screenshot(screenshotsFolder, screenshotsSubFolder);
 
         // Click the Send (Message) button.
         new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/btn_send"))).click();       
-        screenshot(screenshotsFolder);
+        screenshot(screenshotsFolder, screenshotsSubFolder);
         
         // Return to main menu and Log out
         new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/backTextView"))).click();   
-        screenshot(screenshotsFolder);
+        screenshot(screenshotsFolder, screenshotsSubFolder);
         new WebDriverWait(driver, 20).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/backTextView"))).click(); 
-        screenshot(screenshotsFolder);
+        screenshot(screenshotsFolder, screenshotsSubFolder);
         new WebDriverWait(driver, 50).until(ExpectedConditions.presenceOfElementLocated(By.id("com.mindshare.magnifi:id/drawable_left_icon"))).click();
-        screenshot(screenshotsFolder);
+        screenshot(screenshotsFolder, screenshotsSubFolder);
         try 
         {
 		  logout();
-	      screenshot(screenshotsFolder);
+	      screenshot(screenshotsFolder, screenshotsSubFolder);
         } 
         catch (InterruptedException e) 
         {
@@ -68,6 +70,6 @@ public class SendChatMessages extends BaseClass {
         {
 		  e.printStackTrace();
 		}               
-        screenshot(screenshotsFolder);        
+        screenshot(screenshotsFolder, screenshotsSubFolder);        
     }
 }
